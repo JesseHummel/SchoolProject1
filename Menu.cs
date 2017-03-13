c9e6c091a1df78011ab71a400f5e6b2d87ae288a
JesseHummel <jessehummel51@hotmail.com>
2017-03-13 19:04:59 -0400

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FSPG;
+
+namespace ConsoleApplication5
+{
+    public class Menu
+    {
+       public Menu()
+        {
+
+            public string MainMenu();
+      
+
+            Utility.GetKeyState(ConsoleKey.A);
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FSPG;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu MainMenu = new Menu();
+            Board GameBoard = new Board();
+            Settings GameSettings = new Settings();
+
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+
+            Console.WriteLine("WELCOME TO MINESWEEPER!!");
+
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2 + 4);
+            Console.WriteLine("Choose an option!");
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2 + 5);
+            Console.WriteLine("1) START GAME");
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2 + 6);
+            Console.WriteLine("2) Difficulty Setting");
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2 + 7);
+            Console.WriteLine("3) EXIT:'(");
+
+            Console.ReadLine();
+
+        }
+    }
+}
